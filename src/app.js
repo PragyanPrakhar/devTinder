36a4f52176eb96e5f208f956b010548e0b788d33
Pragyan Prakhar <pragyanprakhar@gmail.com>
2024-09-19 20:18:48 +0530

    Exploring http methods

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 
-app.use("/test",(req,res)=>{
+// app.use("/",(req,res)=>{
+//     res.send("Hello Pragyan !!");
+// })
+
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Pragyan", lastName: "Prakhar" });
+});
+
+app.post("/user",(req,res)=>{
+    console.log("Save data to the database");
+    res.send("Data has been saved successfully");
+})
+
+app.delete("/user",(req,res)=>{
+    console.log("Delete data from the database");
+    res.send("Data has been deleted successfully");
+})
+
+app.use("/test", (req, res) => {
     res.send("Hello from the server");
 });
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
