fb3e26e00d18b86f9fcdd7e28331ae578b49e37e
Pragyan Prakhar <pragyanprakhar@gmail.com>
2024-09-28 02:45:13 +0530

    Added the validation of sendConnectionRequest

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const connectionRequestSchema = new mongoose.Schema(
+    {
+        fromUserId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        toUserId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+            enum: {
+                values: ["ignore", "interested", "accepted", "rejected"],
+                messgae: `{VALUE} is incorrect status type`,
+            },
+        },
+    },
+    { timestamps: true }
+);
+
+//This will be called just before everytime the connectionRequest will be save
+connectionRequestSchema.pre("save", function (next ) {
+    const connectionRequest = this;
+    //check if fromUserId is same as toUserId
+    if (connectionRequest.fromUserId.equals(toUserId)) {
+        throw new Error("Can't send Connection Request to YourSelf");
+    }
+    next();
+});
+const connectionRequestModel = new mongoose.model(
+    "ConnectionRequest",
+    connectionRequestSchema
+);
+module.exports = connectionRequestModel;
