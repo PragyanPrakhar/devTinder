70d64485256c231c081ea6b1336a9a4fe8154fa4
Pragyan Prakhar <pragyanprakhar@gmail.com>
2025-01-20 14:47:36 +0530

    Completed the Socket IO integration process

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,12 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const createSecretRoomId = ({ userId, targetUserId }) => {
+    return crypto
+        .createHash("sha256")
+        .update([userId, targetUserId].sort().join("_"))
+        .digest("hex");
+};
 const initializeSocket = (server) => {
     const io = socket(server, {
         cors: {
@@ -8,8 +16,19 @@ const initializeSocket = (server) => {
 
     io.on("connection", (socket) => {
         // Handle Events
-        socket.on("joinchat", () => {});
-        socket.on("sendMessage", () => {});
+        socket.on("joinChat", ({ userId, targetUserId }) => {
+            const roomId = createSecretRoomId(userId, targetUserId);
+            console.log("Joining Room :-> ", roomId);
+            socket.join(roomId);
+        });
+        socket.on(
+            "sendMessage",
+            ({ firstName, userId, targetUserId, text }) => {
+                const roomId = createSecretRoomId(userId, targetUserId);
+                console.log(firstName + " " + text);
+                io.to(roomId).emit("messageReceived", { firstName, text });
+            }
+        );
         socket.on("disconnect", () => {});
     });
 };
